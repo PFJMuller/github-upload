d4dc1da452445fa1e3ff2b19e6cc7a55a00c086d
PFJMuller <info@mullerwenskaarten.nl>
2021-05-10 16:15:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION['menu'])){
+	$menu = $_GET['menu'];
+}
+else{
+	$menu = A;
+}
+include('modules/connect.php');
+include ('modules/header.php');
+include('modules/style.php');
+include ('modules/menu.php');
+if($menu == 'A'){
+	include('modules/home.php');
+	}
+if($menu == 'B'){
+	include('modules/recepten.php');
+}
+if($menu == 'C'){
+	include('modules/ingredienten.php');
+}
+if($menu == 'D'){
+	include('modules/over.php');
+}
+if($menu > 0){
+	include('modules/content.php');
+}
+include('modules/footer.php');
+?>
